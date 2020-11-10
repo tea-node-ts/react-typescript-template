bc245c3494c6375589538829ebf2e9ac8c00f5e1
baiyu <baiyu@miaozhen.com>
2020-11-10 16:30:54 +0800

    fix

--- README.md
+++ README.md
@@ -23,3 +23,29 @@
 地址：https://github.com/formium/tsdx
 
 推荐：5星
+
+### create-react-library
+
+特性：
+
+* 简易的CLI
+* 浏览器兼容性好
+* 支持commonjs和es模块格式
+* 使用create-react-app工具
+* 使用Rollup打包工具
+* 支持Babel
+* 使用Jest测试
+* 支持peer-dependencies
+* 支持css模块
+* 可选TypeScript
+* 创建sourcemap
+* 模块资源
+* 支持文档
+* 中文文档
+* 库包支持
+
+不足：
+
+* 我个人不喜欢style.xxx这种方式使用css
+
+推荐：4星
--- tsdx_example/src/index.tsx
+++ tsdx_example/src/index.tsx
@@ -1,7 +1,40 @@
 import * as React from 'react';
 import './index.scss';
 
-// Delete me
-export const Thing = () => {
-  return <div className="thing">the snozzberries taste like snozzberries</div>;
+interface Props {
+  name: string;
+}
+
+const format = (time: Date) => {
+  const year = time.getFullYear();
+  const month = time.getMonth() + 1;
+  const date = time.getDate();
+
+  const hours = time.getHours() < 10 ? `0${time.getHours()}` : time.getHours();
+  const minites =
+    time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes();
+  const seconds =
+    time.getSeconds() < 10 ? `0${time.getSeconds()}` : time.getSeconds();
+
+  return `${year}-${month}-${date} ${hours}:${minites}:${seconds}`;
+};
+
+export const Hello = ({ name }: Props) => {
+  const [time, setTime] = React.useState(new Date(Date.now()));
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date(Date.now()));
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div className="hello">
+      Hi, {name}! Time is {format(time)}!
+    </div>
+  );
 };
